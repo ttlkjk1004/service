ffd475a953ecc63bdac75f7ea620d6f2044ef4f5
ittlkjk <ittlkjk@gmail.com>
2026-01-06 17:26:12 +0900

    Configure server to serve React app and add build script

--- server/index.js
+++ server/index.js
@@ -234,6 +234,17 @@ app.delete('/api/reports/:id', (req, res) => {
     });
 });
 
+const path = require('path');
+
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
