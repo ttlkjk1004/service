503e19a71a3dfc97a0c0bc689e3cb97247329078
ittlkjk <ittlkjk@gmail.com>
2026-01-06 17:40:43 +0900

    Fix Express 5 wildcard route issue for deployment

--- server/index.js
+++ server/index.js
@@ -241,7 +241,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
